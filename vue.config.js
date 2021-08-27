e4f73173c3d6569496173469c133923de1415f6c
SamuelTribeUK <SamuelTribeUK@outlook.com>
2021-08-27 10:00:15 +0100

    Removed version argument from vue config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = 'F1 Standings';
-      args[1].version = '1.3.0';
       return args;
     });
   }
